9fd82b49b24b4f63a8580ca74b3dde8b95454688
arijit mitra <arijitmitra75@gmail.com>
2016-11-17 18:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req,res){
-    var articleName=req.params.articlename;
+    var articleName=req.params.articleName;
     res.send(createtemplate(articles[articleName]));
 }
 );
