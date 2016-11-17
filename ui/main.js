d62ea21eaa35da047f1cdf7b4b909ce779989e18
arijit mitra <arijitmitra75@gmail.com>
2016-11-17 21:15:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,8 @@
-console.log('Loaded!');
-var element= document.getElementById('maintext');
-element.innerHTML ="arijit";
-
-//move the image
-var img= document.getElementById('image');
-var marginLeft = 0;
-function moveRight(){   
-    marginLeft = marginLeft +5;
-    img.style.marginLeft = marginLeft +'px';
+var button=document.getElementByid("counter");
+var counter=0;
+button.onClick=  function(){
     
-}
-img.onclick = function(){
-    
-    
-    var interval= setInterval(moveRight,50);
-    
+    counter=counter+1;
+    var span= document.getElementById("count");
+    span.innerHTML=counter.toString();
 };
